bb769acd63d56769ffd998f2ee9abdbd39dd5fb3
Marcus Orochena <marcus.orochena@gmail.com>
2021-11-11 00:39:36 -0500

    cloudron config

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+if ENV['CLOUDRON']
+  ENV['BOOTSNAP_CACHE_DIR'] = '/tmp/cache'
+end
+
 require "bundler/setup" # Set up gems listed in the Gemfile.
 require "bootsnap/setup" # Speed up boot time by caching expensive operations.
