5bd49b6774412cc2c720daa25d887dbcc5d8b615
Marcus Orochena <marcus.orochena@gmail.com>
2021-12-17 00:50:48 -0500

    1.0.8 - added footer, indicate when projects are voted on

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 VERSION=$(cat ./.version)
+
+sed -i 's/"version":.*/"version": "'$VERSION'",/' CloudronManifest.json
+
 docker build -t morochena/psquare:$VERSION . && docker push morochena/psquare:$VERSION
 cloudron update --app=psquare --image=morochena/psquare:$VERSION
